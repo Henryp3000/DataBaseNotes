68a91c3429ec6aa5c163c8c3d42c7c3ae208742a
Henryp3000 <Henrypereira30@gmail.com>
2022-10-25 22:01:27 -0400

    databaseNotes

---
+++ src/main/java/Notes.java
@@ -0,0 +1,50 @@
+public class Notes {
+    /*
+    What is a database
+    - An organized collection of structured information or data
+
+    The difference between a database and a spreadsheet
+
+    Types of databases
+    Object-oriented databases
+    -Information in an object-oriented database is represented in the form of objects, as in object-oriented programming.
+
+    Distributed databases
+    - A distributed database consists of two or more files located in different sites. The database may be stored on multiple computers,
+     located in the same physical location, or scattered over different networks.
+    Data warehouses
+    - A central repository for data, a data warehouse is a type of database specifically designed for fast query and analysis.
+    NoSQL databases
+    -A NoSQL, or nonrelational database, allows unstructured and semistructured data to be stored and manipulated (in contrast to a relational database,
+     which defines how all data inserted into the database must be composed). NoSQL databases grew popular as web applications became more common and more complex.
+    Graph databases
+    - A graph database stores data in terms of entities and the relationships between entities.
+    OLTP databases.
+    - An OLTP database is a speedy, analytic database designed for large numbers of transactions performed by multiple users.
+    Open source databases
+    - An open source database system is one whose source code is open source; such databases could be SQL or NoSQL databases.
+    Cloud databases
+    - A cloud database is a collection of data, either structured or unstructured, that resides on a private, public, or hybrid cloud computing platform.
+    There are two types of cloud database models: traditional and database as a service (DBaaS). With DBaaS, administrative tasks and maintenance are performed by a service provider.
+    Multimodel database
+    - Multimodel databases combine different types of database models into a single, integrated back end. This means they can accommodate various data types.
+    Document/JSON database
+    Designed for storing, retrieving, and managing document-oriented information, document databases are a modern way to store data in JSON format rather than rows and columns.
+    Self-driving databases
+    The newest and most groundbreaking type of database, self-driving databases (also known as autonomous databases) are cloud-based and use machine learning to automate database tuning,
+    security, backups, updates, and other routine management tasks traditionally performed by database administrators.
+    database Managment system
+    - A database serves as an interface between the database and its end users or programs, allowing users to retrieve, update, and manage how the information is organized and optimized.
+     A DBMS also facilitates oversight and control of databases, enabling a variety of administrative operations such as performance monitoring, tuning, and backup and recovery.
+
+     Some examples of popular database software or DBMSs include MySQL,
+     Microsoft Access, Microsoft SQL Server, FileMaker Pro, Oracle Database, and dBASE.
+
+     MYSQL database
+    - MySQL is an open source relational database management system based on SQL.
+     created to optimized for web applications and can run on any platform.
+
+     - MySQL is the DBMS behind some of the top websites and web-based applications in the world,
+     including Airbnb, Uber, LinkedIn, Facebook, Twitter, and YouTube.
+     */
+}
